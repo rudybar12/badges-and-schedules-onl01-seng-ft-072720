1d2a05108695a461bf6d177f2da88e0f09932a1f
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-08-02 07:56:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each do |name|
+    badge_messages.push("Hello, my name is #{name}.")
+  end
+  badge_messages
+end
+
+def assign_rooms(names)
+  room_assignments = []
+  n = 1
+  names.each_with_index do |name, index|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+    n +=1
+  end
+  room_assignments
+end
